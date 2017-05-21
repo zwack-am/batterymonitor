7f5ac8fb4b01c566a90b3f968fef29ea8acb6dba
zwack-am <zwack.am@gmail.com>
2017-05-20 22:10:19 -0700

    Fixed quoting in the script

--- install_battery_monitor.sh
+++ install_battery_monitor.sh
@@ -9,12 +9,12 @@ fi
 cat > /usr/local/bin/batterymonitor.sh << EOF
 #! /bin/sh
 
-FUEL=$((`/usr/sbin/i2cget -y -f 0 0x34 0xb9`))
+FUEL=\$((\`/usr/sbin/i2cget -y -f 0 0x34 0xb9\`))
 
-if [ $FUEL -eq 127 ]
+if [ \$FUEL -eq 127 ]
 then
   echo "default-on" > /sys/class/leds/chip\:white\:status/trigger
-elif [ $FUEL -le 25 ]
+elif [ \$FUEL -le 25 ]
 then
   echo "heartbeat" > /sys/class/leds/chip\:white\:status/trigger
 else
