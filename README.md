16c4cc784933f2b78c95a1c1f628c47e844c79ab
zwack-am <zwack.am@gmail.com>
2017-05-20 21:50:18 -0700

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # batterymonitor
+
+A simple battery monitor that turns the status LED on at startup, off until 25% remains and then flashes it.
+
+To install simply run
+
+sudo install_battery_monitor.sh
+
---
+++ install_battery_monitor.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+if  [ $EUID -ne 0 ]
+then
+  echo This must be run as root
+  exit 1
+fi
+
+cat > /usr/local/bin/batterymonitor.sh << EOF
+#! /bin/sh
+
+FUEL=$((`/usr/sbin/i2cget -y -f 0 0x34 0xb9`))
+
+if [ $FUEL -eq 127 ]
+then
+  echo "default-on" > /sys/class/leds/chip\:white\:status/trigger
+elif [ $FUEL -le 25 ]
+then
+  echo "heartbeat" > /sys/class/leds/chip\:white\:status/trigger
+else
+  echo "none" > /sys/class/leds/chip\:white\:status/trigger
+fi 
+EOF
+
+chmod 0755 /usr/local/bin/battermonitor.sh
+
+crontab -l > /tmp/rootcrontab
+
+echo "*/5 * * * * /usr/local/bin/batterymonitor.sh" >> /tmp/rootcrontab
+
+crontab /tmp/rootcrontab
+
+rm /tmp/rootcrontab
